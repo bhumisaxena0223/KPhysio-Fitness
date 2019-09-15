51c528b39a3ae73260d069fda7e8d2171ebe9112
bhumi <bhumisaxena0223@gmail.com>
2019-09-15 12:47:46 +0530

    Bootstarp added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,9 @@
 console.log("Hello world");
-document.getElementById("demo").innerHTML = "My First JavaScript";
+// // document.getElementById("demo").innerHTML = "My First JavaScript";
+// function  blur(dd)
+// {
+//     console.log("workingggg", dd)
+// }
+// function myFunction() {
+//     document.getElementById("demo").innerHTML = "Paragraph changed.";
+//   }
